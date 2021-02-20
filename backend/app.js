2fce61226b2e6455d96cf7c877beb46ccee4fac7
gregriedo <gregory.riedo@gmailcom>
2021-02-20 15:40:39 +0100

    ajout du fichier app.js dans dossier backend

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect = require('./connexion');
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
